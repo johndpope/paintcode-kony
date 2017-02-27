9b094e80b499df4349d3c0a4d1012c0cf777a854
Fernando Fernandes <mfernandes.fernando@gmail.com>
2017-02-27 08:47:12 -0300

    Created Xcode project.

---
+++ iOS/SwiftFFI/Supporting Files/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  SwiftFFI
+//
+//  Created by Fernando Fernandes on 2/27/17.
+//  Copyright © 2017 Sicredi. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
